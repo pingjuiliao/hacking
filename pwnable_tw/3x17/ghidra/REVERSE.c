618f797e8c40f83ec20905c0d1651e4aa548edac
pingjui <pingjuiliao@gmail.com>
2023-03-01 02:53:21 -0800

    3x17 REVERSED more writeup

--- pwnable_tw/3x17/ghidra/REVERSE.c
+++ pwnable_tw/3x17/ghidra/REVERSE.c
@@ -1,5 +1,6 @@
 
 // 0x401b6d
+// We can only overwrite data region
 char main(void)
 {
   char cVar1;
@@ -15,7 +16,7 @@ char main(void)
     write(1,"addr:",5); // 0x446ec0
     read(0,local_28,0x18); // 0x446e20
     iVar2 = atoi64(local_28); // 0x44ee70
-    // 0x401bf2
+    // 0x401bf2: cltq
     iVar2 = dword_to_qword_sign(iVar2);
     write(1,"data:",5);
     read(0,(long)iVar2,0x18);
@@ -27,5 +28,26 @@ char main(void)
   }
   // 0x401c4c, esp = 0x7fffffffd988
   return cVar1;
+}
+
+// Some where after return from main...
+// It took so long to find it
+// Ghidra helps a lot.
+//
+// Logic towards finding it:
+// we get to arbitrary write once in the main
+// so the vulnerable part must happen after it.
+// I review instrucion step by step to find the vulnerability
+void FUN_00402960(void)
+{
+  long lVar1;
+  
+  lVar1 = 1;
+  do {
+    (*(code *)(&PTR_FUN_004b40f0)[lVar1])();
+    lVar1 = lVar1 + -1;
+  } while (lVar1 != -1);
+  FUN_0048e32c();
+  return;
 }
 
